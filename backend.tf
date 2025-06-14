1ad0e196447594cb98e6d486e78c8cbed23e3ebb
Muhammad Zohaib Hussain <144581164+zohaib9990@users.noreply.github.com>
2025-06-14 22:17:47 +0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "mario12bucket"  # Ensure this is your actual S3 bucket
+    bucket         = "mario12bucket-1099"  # Ensure this is your actual S3 bucket
     key            = "eks/terraform.tfstate"  # Standardized directory naming
     region         = "us-east-1"
     encrypt        = true  # Ensures state file is encrypted
